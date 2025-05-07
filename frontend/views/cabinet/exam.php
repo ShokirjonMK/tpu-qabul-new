b3f422eeec57a588d8f8192ab908aa497018d260
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-07 19:13:51 +0500

    Update exam.php

--- frontend/views/cabinet/exam.php
+++ frontend/views/cabinet/exam.php
@@ -169,7 +169,7 @@ if ($eduDirection->is_oferta == 1) {
                                                     <p><?= $directionSubject->ball ?? "0" ?> ball</p>
                                                 </li>
                                             </ul>
-                                            <?php if ($exam->status > 3) : ?>
+                                            <?php if ($exam->status > 3 && $exam->ball > 65) : ?>
                                                 <ul>
                                                     <li>Fandan to'plagan ball:</li>
                                                     <li>
