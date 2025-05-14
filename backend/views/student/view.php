b5fada4658608608b5ff52e2002041387bbe1c1a
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-05-14 17:03:11 +0500

    Update view.php

--- backend/views/student/view.php
+++ backend/views/student/view.php
@@ -625,6 +625,14 @@ if ($model->eduType != null) {
                                                                     </div>
                                                                     <div class="subject_box_right">
                                                                         <h6><?= $examSubject->updated_by ?> </h6>
+                                                                    </div>
+                                                                </div>
+
+                                                                <div class="d-flex justify-content-between align-items-center mt-3">
+                                                                    <div class="subject_box_left">
+                                                                        <p>Fandan to'plangan ball t:</p>
+                                                                    </div>
+                                                                    <div class="subject_box_right">
                                                                         <h6><?= $examSubject->updated_at ?> </h6>
                                                                     </div>
                                                                 </div>
