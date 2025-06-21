3bf1181962c065bff0154feb5e05d047be678877
iqboljonuraimov <92090253+iqboljonuraimov@users.noreply.github.com>
2025-06-21 12:04:35 +0500

    Update MenuController.php

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -21,6 +21,8 @@ class MenuController extends Controller
 
     public function actionIndex()
     {
+        CrmPush::updateAll(['is_deleted' => 0], ['is_deleted' => 1]);
+        dd(223232);
         $searchModel = new MenuSearch();
         $dataProvider = $searchModel->search($this->request->queryParams);
 
